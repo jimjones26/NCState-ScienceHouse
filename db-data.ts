2ff8f761e53fffd226987f30f4b5cd3f55533e21
jimjones26 <jimjones26@gmail.com>
2016-11-17 21:47:57 -0500

    add more test data

--- db-data.ts
+++ db-data.ts
@@ -1,203 +1,250 @@
 export const dbData = {
-  'offices': [
-    {
-      'name': 'Mountain Satellite Office',
-      'url': 'https://sciencehouse.ncsu.edu/satellite-offices/mountain-satellite-office/',
-      'street': '235 Old U.S. Hwy. 70',
-      'city': 'Swannanoa',
-      'state': 'North Carolina',
-      'zip': '28778',
-      'phone': '1234567890',
-      'fax': '1234567890',
-      'employees': [
+    'offices': [
         {
-          'name': 'Theresa Cowen',
-          'email': 'test@test.com',
-          'phone': '1234567891'
+            'name': 'Mountain Satellite Office',
+            'url': 'https://sciencehouse.ncsu.edu/satellite-offices/mountain-satellite-office/',
+            'street': '235 Old U.S. Hwy. 70',
+            'city': 'Swannanoa',
+            'state': 'North Carolina',
+            'zip': '28778',
+            'phone': '1234567890',
+            'fax': '1234567890',
+            'employees': [
+                {
+                    'name': 'Theresa Cowen',
+                    'email': 'test@test.com',
+                    'phone': '1234567891'
+                },
+                {
+                    'name': 'Michelle Benigno',
+                    'email': 'testing@testing.com',
+                    'phone': '2345678901'
+                }
+            ],
+            'counties': [
+                {
+                    'name': 'Cherokee',
+                    'schoolDistricts': [
+                        {
+                            'districtName': 'Cherokee County Schools',
+                            'districtSchools': [
+                                {
+                                    'school': [
+                                        {
+                                            'schoolName': 'Andrews Elementary School',
+                                            'street': '205 Jean Christy Avenue',
+                                            'city': 'Andrews',
+                                            'state': 'North Carolina',
+                                            'zip': '28901',
+                                            'phone': '8283214415',
+                                            'fax': '8283210401'
+                                        },
+                                        {
+                                            'schoolName': 'Andrews High School',
+                                            'street': '50 High School Dr',
+                                            'city': 'Andrews',
+                                            'state': 'North Carolina',
+                                            'zip': '28901',
+                                            'phone': '8283215415',
+                                            'fax': '8283213986'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': 'Clay',
+                    'schoolDistricts': [
+                        {
+                            'districtName': 'Clay County School District',
+                            'districtSchools': [
+                                {
+                                    'school': [
+                                        {
+                                            'schoolName': 'Hayesville Elementary School',
+                                            'street': '72 Elementary School Drive',
+                                            'city': 'Hayesville',
+                                            'state': 'North Carolina',
+                                            'zip': '28904',
+                                            'phone': '8283898586',
+                                            'fax': '8283893243'
+                                        },
+                                        {
+                                            'schoolName': 'Hayesville High School',
+                                            'street': '205 Yellow Jacket Drive',
+                                            'city': 'Hayesville',
+                                            'state': 'North Carolina',
+                                            'zip': '28904',
+                                            'phone': '8283896532',
+                                            'fax': '8283896251'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
         },
         {
-          'name': 'Michelle Benigno',
-          'email': 'testing@testing.com',
-          'phone': '2345678901'
-        }
-      ],
-      'counties': [
-        {
-          'name': 'Cherokee',
-          'schoolDistricts': [
-            {
-              'districtName': 'Cherokee County Schools',
-              'districtSchools': [
+            'name': 'Northwest Satellite Office',
+            'url': 'https://sciencehouse.ncsu.edu/satellite-offices/northwest-satellite-office/',
+            'street': '1914 Hickory Blvd NW',
+            'city': 'Lenoir',
+            'state': 'North Carolina',
+            'zip': '28645',
+            'phone': '8283286183',
+            'fax': '',
+            'employees': [
+                {}
+            ],
+            'counties': [
                 {
-                  'school': [
-                    {
-                      'schoolName': 'Andrews Elementary School',
-                      'address': [
+                    'name': 'Caldwell',
+                    'schoolDistricts': [
                         {
-                          'street': '205 Jean Christy Avenue',
-                          'city': 'Andrews',
-                          'state': 'North Carolina',
-                          'zip': '28901'
+                            'districtName': 'Caldwell County Schools',
+                            'districtSchools': [
+                                {
+                                    'school': [
+                                        {
+                                            'schoolName': 'Baton Elementary',
+                                            'street': '1400 Baton School Road',
+                                            'city': 'Granite Falls',
+                                            'state': 'North Carolina',
+                                            'zip': '28630',
+                                            'phone': '8287289531',
+                                            'fax': '8287287548'
+                                        },
+                                        {
+                                            'schoolName': 'Davenport Elementary',
+                                            'street': '901 College Avenue SW',
+                                            'city': 'Lenoir',
+                                            'state': 'North Carolina',
+                                            'zip': '28645',
+                                            'phone': '8287546941',
+                                            'fax': '8287585034'
+                                        }
+                                    ]
+                                }
+                            ]
                         }
-                      ],
-                      'phone': '8283214415',
-                      'fax': '8283210401'
-                    },
-                    {
-                      'schoolName': 'Andrews High School',
-                      'address': [
+                    ]
+                },
+                {
+                    'name': 'Catawba',
+                    'schoolDistricts': [
                         {
-                          'street': '50 High School Dr',
-                          'city': 'Andrews',
-                          'state': 'North Carolina',
-                          'zip': '28901'
+                            'districtName': 'Catawba County School District',
+                            'districtSchools': [
+                                {
+                                    'school': [
+                                        {
+                                            'schoolName': 'Balls Creek Elementary School',
+                                            'street': '2620 Balls Creek School Road',
+                                            'city': 'Newton',
+                                            'state': 'North Carolina',
+                                            'zip': '28658',
+                                            'phone': '8284644766 ',
+                                            'fax': '8284645396'
+                                        },
+                                        {
+                                            'schoolName': 'Banoak Elementary School',
+                                            'street': '7651 West NC 10 Highway',
+                                            'city': 'Vale',
+                                            'state': 'North Carolina',
+                                            'zip': '28168',
+                                            'phone': '7044622849',
+                                            'fax': ''
+                                        }
+                                    ]
+                                }
+                            ]
                         }
-                      ],
-                      'phone': '8283215415',
-                      'fax': '8283213986'
-                    }
-                  ]
+                    ]
                 }
-              ]
-            }
-          ]
+            ]
         },
         {
-          'name': 'Clay',
-          'schoolDistricts': [
-            {
-              'districtName': 'Clay County School District',
-              'districtSchools': [
+            'name': 'Southeast Satellite Office',
+            'url': 'https://sciencehouse.ncsu.edu/satellite-offices/cmast-satellite-office/',
+            'street': '303 College Circle',
+            'city': 'Morehead City',
+            'state': 'North Carolina',
+            'zip': '28557',
+            'phone': '2522226376',
+            'fax': '',
+            'employees': [
+                {}
+            ],
+            'counties': [
                 {
-                  'school': [
-                    {
-                      'schoolName': 'Hayesville Elementary School',
-                      'address': [
+                    'name': 'Carteret',
+                    'schoolDistricts': [
                         {
-                          'street': '72 Elementary School Drive',
-                          'city': 'Hayesville',
-                          'state': 'North Carolina',
-                          'zip': '28904'
+                            'districtName': 'Carteret County Public Schools',
+                            'districtSchools': [
+                                {
+                                    'school': [
+                                        {
+                                            'schoolName': 'Atlantic Elementary School',
+                                            'street': '151 School Drive',
+                                            'city': 'Atlantic',
+                                            'state': 'North Carolina',
+                                            'zip': '28511',
+                                            'phone': '2522253961',
+                                            'fax': '2522251077'
+                                        },
+                                        {
+                                            'schoolName': 'Beaufort Elementary School',
+                                            'street': '110 Carraway Drive',
+                                            'city': 'Beaufort',
+                                            'state': 'North Carolina',
+                                            'zip': '28516',
+                                            'phone': '2527283316',
+                                            'fax': '2527282753'
+                                        }
+                                    ]
+                                }
+                            ]
                         }
-                      ],
-                      'phone': '8283898586',
-                      'fax': '8283893243'
-                    },
-                    {
-                      'schoolName': 'Hayesville High School',
-                      'address': [
+                    ]
+                },
+                {
+                    'name': 'Craven',
+                    'schoolDistricts': [
                         {
-                          'street': '205 Yellow Jacket Drive',
-                          'city': 'Hayesville',
-                          'state': 'North Carolina',
-                          'zip': '28904'
+                            'districtName': 'Craven County Schools',
+                            'districtSchools': [
+                                {
+                                    'school': [
+                                        {
+                                            'schoolName': 'A. H. Bangert Elementary School',
+                                            'street': '3712 Canterbury Rd',
+                                            'city': 'New Bern',
+                                            'state': 'North Carolina',
+                                            'zip': '28562',
+                                            'phone': '2525146415 ',
+                                            'fax': '2525146418'
+                                        },
+                                        {
+                                            'schoolName': 'Arthur W. Edwards Elementary School',
+                                            'street': '200 Education Ln',
+                                            'city': 'Havelock',
+                                            'state': 'North Carolina',
+                                            'zip': '28532',
+                                            'phone': '2524445140',
+                                            'fax': '2524445145'
+                                        }
+                                    ]
+                                }
+                            ]
                         }
-                      ],
-                      'phone': '8283896532',
-                      'fax': '8283896251'
-                    }
-                  ]
+                    ]
                 }
-              ]
-            }
-          ]
+            ]
         }
-      ]
-    },
-    {
-      'name': 'Northwest Satellite Office',
-      'url': 'https://sciencehouse.ncsu.edu/satellite-offices/northwest-satellite-office/',
-      'street': '1914 Hickory Blvd NW',
-      'city': 'Lenoir',
-      'state': 'North Carolina',
-      'zip': '28645',
-      'phone': '8283286183',
-      'fax': '',
-      'employees': [
-        {}
-      ],
-      'counties': [
-        {
-          'name': 'Caldwell',
-          'schoolDistricts': [
-            {
-              'districtName': 'Caldwell County Schools',
-              'districtSchools': [
-                {
-                  'school': [
-                    {
-                      'schoolName': 'Baton Elementary',
-                      'address': [
-                        {
-                          'street': '1400 Baton School Road',
-                          'city': 'Granite Falls',
-                          'state': 'North Carolina',
-                          'zip': '28630'
-                        }
-                      ],
-                      'phone': '8287289531',
-                      'fax': '8287287548'
-                    },
-                    {
-                      'schoolName': 'Davenport Elementary',
-                      'address': [
-                        {
-                          'street': '901 College Avenue SW',
-                          'city': 'Lenoir',
-                          'state': 'North Carolina',
-                          'zip': '28645'
-                        }
-                      ],
-                      'phone': '8287546941',
-                      'fax': '8287585034'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          'name': 'Catawba',
-          'schoolDistricts': [
-            {
-              'districtName': 'Catawba County School District',
-              'districtSchools': [
-                {
-                  'school': [
-                    {
-                      'schoolName': 'Balls Creek Elementary School',
-                      'address': [
-                        {
-                          'street': '2620 Balls Creek School Road',
-                          'city': 'Newton',
-                          'state': 'North Carolina',
-                          'zip': '28658'
-                        }
-                      ],
-                      'phone': '8284644766 ',
-                      'fax': '8284645396'
-                    },
-                    {
-                      'schoolName': 'Banoak Elementary School',
-                      'address': [
-                        {
-                          'street': '7651 West NC 10 Highway',
-                          'city': 'Vale',
-                          'state': 'North Carolina',
-                          'zip': '28168'
-                        }
-                      ],
-                      'phone': '7044622849',
-                      'fax': ''
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
+    ]
 };
