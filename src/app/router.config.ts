1dfaa9a97aa8fd2f43e1e2ff7e2cef10af256a91
jimjones26 <jimjones26@gmail.com>
2016-11-17 20:04:25 -0500

    stub out basic router config and top meu layout

--- src/app/router.config.ts
+++ src/app/router.config.ts
@@ -1,9 +1,13 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OfficesListComponent } from './offices-list/offices-list.component';
 
 export const routerConfig: Route[] = [
   {
     path: 'home', component: HomeComponent
+  },
+  {
+    path: 'test', component: OfficesListComponent
   },
   {
     path: '', redirectTo: 'home', pathMatch: 'full'
