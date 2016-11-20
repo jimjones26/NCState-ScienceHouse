6b7ffb3d746444c64b15da09d6c360f7f901a908
jimjones26 <jimjones26@gmail.com>
2016-11-19 22:48:22 -0500

    complete county model

---
+++ src/app/shared/model/county.ts
@@ -0,0 +1,16 @@
+export class County {
+
+  static fromJson($key, name) {
+    return new County($key, name);
+  }
+
+  static fromJsonArray(json: any[]): County[] {
+    return json.map(County.fromJson);
+  }
+
+  constructor(
+    public $key: string,
+    public name: string
+  ) { }
+
+}
