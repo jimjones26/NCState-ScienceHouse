568e3ece304d4134dd0dcdd43ca9640c57cce222
jimjones26 <jimjones26@gmail.com>
2016-11-24 21:55:13 -0500

    movies counties list into it's own component

--- src/app/county-list/county-list.component.ts
+++ src/app/county-list/county-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { County } from '../shared/model/county';
 
 @Component({
   selector: 'app-county-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./county-list.component.css']
 })
 export class CountyListComponent implements OnInit {
+
+  @Input()
+  counties: County[];
 
   constructor() { }
 
