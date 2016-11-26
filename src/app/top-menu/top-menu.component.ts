4f8b0dc3f6a7c980ab2810cda1fe714295e615a6
jimjones26 <jimjones26@gmail.com>
2016-11-26 15:57:20 -0500

    add redirect to home on logout

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AuthService } from '../shared/security/auth.service';
 import { AuthInfo } from '../shared/security/auth-info';
 
@@ -11,7 +13,7 @@ export class TopMenuComponent implements OnInit {
 
   authInfo: AuthInfo;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.authService.authInfo$.subscribe(authInfo => this.authInfo = authInfo);
@@ -19,6 +21,7 @@ export class TopMenuComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+    this.router.navigate(['/home']);
   }
 
 }
