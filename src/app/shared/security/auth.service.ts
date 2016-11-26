53769231c598f84b7420764c24645f9a2a479128
jimjones26 <jimjones26@gmail.com>
2016-11-26 12:40:08 -0500

    implement login functionality and UI

--- src/app/shared/security/auth.service.ts
+++ src/app/shared/security/auth.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs/Rx';
+import { FirebaseAuth, FirebaseAuthState } from 'angularfire2/index';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: FirebaseAuth) { }
+
+  login(email, password): Observable<FirebaseAuthState> {
+    return this.fromFirebaseAuthPromise(this.auth.login({ email, password }));
+  }
+
+  fromFirebaseAuthPromise(promise): Observable<any> {
+    const subject = new Subject<any>();
+
+    promise
+      .then(res => {
+        subject.next(res);
+        subject.complete();
+      }, err => {
+        subject.error(err);
+        subject.complete();
+      });
+
+    return subject;
+  }
 
 }
