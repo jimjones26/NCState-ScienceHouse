ddb5ddde0354b1a11020371fd2fc50dce1c19e7c
jimjones26 <jimjones26@gmail.com>
2016-11-26 18:15:56 -0500

    update auth service to check for current user

--- src/app/shared/security/auth.service.ts
+++ src/app/shared/security/auth.service.ts
@@ -6,41 +6,53 @@ import { AuthInfo } from './auth-info';
 @Injectable()
 export class AuthService {
 
-  static UNKNOWN_USER = new AuthInfo(null);
+    static UNKNOWN_USER = new AuthInfo(null);
 
-  authInfo$: BehaviorSubject<AuthInfo> = new BehaviorSubject<AuthInfo>(AuthService.UNKNOWN_USER);
+    authInfo$: BehaviorSubject<AuthInfo> = new BehaviorSubject<AuthInfo>(AuthService.UNKNOWN_USER);
 
-  constructor(private auth: FirebaseAuth) { }
+    constructor(private auth: FirebaseAuth) {
+        this.auth.subscribe(user => {
+            if (user) {
+                console.log('we have a user!');
+                console.log(user.uid + user.auth.email);
+                const authInfo = new AuthInfo(user.uid);
+                this.authInfo$.next(authInfo);
+            } else {
+                console.log('aint no user around here');
+                this.authInfo$.next(AuthService.UNKNOWN_USER);
+            }
+        });
+    }
 
-  login(email, password): Observable<FirebaseAuthState> {
-    return this.fromFirebaseAuthPromise(this.auth.login({ email, password }));
-  }
+    login(email, password): Observable<FirebaseAuthState> {
+        return this.fromFirebaseAuthPromise(this.auth.login({ email, password }));
+    }
 
-  signUp(email, password) {
-    return this.fromFirebaseAuthPromise(this.auth.createUser({ email, password }));
-  }
+    signUp(email, password) {
+        return this.fromFirebaseAuthPromise(this.auth.createUser({ email, password }));
+    }
 
-  fromFirebaseAuthPromise(promise): Observable<any> {
-    const subject = new Subject<any>();
+    fromFirebaseAuthPromise(promise): Observable<any> {
+        const subject = new Subject<any>();
 
-    promise
-      .then(res => {
-        const authInfo = new AuthInfo(this.auth.getAuth().uid);
-        this.authInfo$.next(authInfo);
-        subject.next(res);
-        subject.complete();
-      }, err => {
-        this.authInfo$.error(err);
-        subject.error(err);
-        subject.complete();
-      });
+        promise
+            .then(res => {
+                const authInfo = new AuthInfo(this.auth.getAuth().uid);
+                this.authInfo$.next(authInfo);
+                subject.next(res);
+                subject.complete();
+            }, err => {
+                this.authInfo$.error(err);
+                subject.error(err);
+                subject.complete();
+            });
 
-    return subject;
-  }
+        return subject;
+    }
 
-  logout() {
-    this.auth.logout();
-    this.authInfo$.next(AuthService.UNKNOWN_USER);
-  }
+    logout() {
+        this.auth.logout();
+        this.authInfo$.next(AuthService.UNKNOWN_USER);
+    }
 
 }
